791bb41a2f0792263dd8162a589865e1e1c4c8b1
Chetan Talwar <thechetantalwar@users.noreply.github.com>
2021-12-15 01:40:38 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-036d46416a34a611c"
+  instance_type = "t2.micro"
+ tags = {
+    "Name" = "policytest"
+}
+}
